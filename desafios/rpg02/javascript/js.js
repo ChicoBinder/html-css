afc3819047ea49fd8b19991242cca61ad7b437ed
Francisco Binder De Lima <chicobinderlima@gmail.com>
2023-05-09 18:41:18 -0300

    Ficha Vórios

--- desafios/rpg02/javascript/js.js
+++ desafios/rpg02/javascript/js.js
@@ -7,3 +7,13 @@ $(document).ready(function() {
            event.stopPropagation();
        });
    });
+
+   $(document).ready(function() {
+    var botao = $('.bt2'); //classe no a clicado
+    var dropDown = $('.ul-fichas'); //classe do submenu do ul que vai arbrir ao clicar
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
