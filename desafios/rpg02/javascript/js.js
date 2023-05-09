c58d0291c6af57024b8aa618451e1ad0649f42bb
Francisco Binder De Lima <chicobinderlima@gmail.com>
2023-05-09 11:17:22 -0300

    Botões Nav

---
+++ desafios/rpg02/javascript/js.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    var botao = $('.bt1'); //classe no a clicado
+    var dropDown = $('.ul-menu'); //classe do submenu do ul que vai arbrir ao clicar
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
